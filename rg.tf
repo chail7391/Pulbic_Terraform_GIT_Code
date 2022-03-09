65dc86edd36abc1f6397fb70eac7839780af7e25
chail7391 <32648307+chail7391@users.noreply.github.com>
2022-03-09 23:46:13 +0530

    Added second Azure region

--- rg.tf
+++ rg.tf
@@ -3,3 +3,8 @@ resource "azurerm_resource_group" "DevopsGIT" {
   name     = "DevopsGIT_rg1"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "DevopsGIT_rg2" {
+  name     = "DevopsGIT_rg2"
+  location = "westus"
+}
